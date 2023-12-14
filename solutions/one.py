750341cd3820816bf4af719e7af7d9c2bfbf0ecd
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2023-12-14 21:08:22 +0000

    working

--- solutions/one.py
+++ solutions/one.py
@@ -5,4 +5,26 @@ with open(input_file, 'r') as f:
     for line in f:
         lines.append(line.rstrip('\n'))
 
-print(lines[:10])
+# print(lines[:10])
+
+strng = lines[0]
+
+finished = False
+p, q = 0, len(strng) - 1
+sum = 0
+nums = 0
+
+# while p < q:
+#     left, right = strng[p], strng[q]
+    
+#     if left.isalpha():
+#         p += 1
+#     else:
+#         sum += left 
+#         nums += 1
+
+#     if right.isalpha(): 
+#         q -= 1
+#     else:
+#         sum += right
+#         nums += 1
