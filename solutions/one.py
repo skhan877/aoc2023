5f37f4ab5d694ba306c1205b6ba71d4d9b680094
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2023-12-17 21:58:06 +0000

    solved

--- solutions/one.py
+++ solutions/one.py
@@ -1,30 +1,40 @@
 input_file = '../inputs/one.txt'
 
+def solution(strng):
+
+    finished = False
+    p, q = 0, len(strng) - 1
+    new_num = []
+
+    while not finished:
+        left = strng[p]
+        
+        if left.isalpha():
+            p += 1
+        else:
+            new_num.append(left)
+            finished = True 
+
+    finished = False 
+    while not finished:
+        right = strng[q]
+
+        if right.isalpha(): 
+            q -= 1
+        else:
+            new_num.append(right)
+            finished = True
+
+    new_num = int(''.join(new_num))
+    return new_num 
+
 with open(input_file, 'r') as f:
     lines = []
     for line in f:
         lines.append(line.rstrip('\n'))
 
-# print(lines[:10])
+result = 0
+for line in lines:
+    result += solution(line)
 
-strng = lines[0]
-
-finished = False
-p, q = 0, len(strng) - 1
-sum = 0
-nums = 0
-
-# while p < q:
-#     left, right = strng[p], strng[q]
-    
-#     if left.isalpha():
-#         p += 1
-#     else:
-#         sum += left 
-#         nums += 1
-
-#     if right.isalpha(): 
-#         q -= 1
-#     else:
-#         sum += right
-#         nums += 1
+print(result) 
