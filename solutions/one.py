d0c2a6dba206d64e3f8fcd734c61472ebc6ab656
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2023-12-24 16:39:40 +0000

    solved

--- solutions/one.py
+++ solutions/one.py
@@ -8,7 +8,6 @@ def solution(strng):
         left = strng[p]
         
         if left.isalpha():
-            # function to check string here, otherwise p+= 1
             p += 1
         else:
             new_num.append(left)
@@ -30,45 +29,10 @@ def solution(strng):
 
 def is_num_str(strng, nums_dict):
     
-    # for char in list(strng):
     for k, v in nums_dict.items():
         strng = strng.replace(k, v)
     return strng
 
-    # finished = False
-    # reversed = False
-    # new_strngs = []
-
-    # while not reversed:
-    #     for char in list(strng):
-    #         for k, v in nums_dict.items():
-    #             if char == k[0]:
-    #                 new_strng = strng.replace(k, str(v))
-    #                 if new_strng != strng: # replacement successful
-    #                     new_strngs.append(new_strng)
-    #                     reversed = True 
-    #                     break
-    #         if reversed:
-    #             break
-    #     break
-
-    # if len(new_strngs) == 0:
-    #     new_strng = strng 
-
-    # while not finished:
-    #     for char in list(new_strng)[::-1]:
-    #         for k, v in nums_dict.items():
-    #             if char == k[-1]:
-    #                 new_new_strng = new_strng.replace(k, str(v))
-    #                 if new_new_strng != new_strng: # replacement successful
-    #                     new_strngs.append(new_new_strng)
-    #                     finished = True
-    #                     break
-    #         if finished:
-    #             break
-    #     break
-
-    # return new_strngs[-1] if len(new_strngs) > 0 else strng
 
 
 def main(): 
@@ -92,19 +56,14 @@ def main():
             'nine' : 'ni9ne'
         }
     
-    sample = ['sndk8', '1111', 'eightwothree', 'eightwo', 'two1nine','abcone2threexyz','xtwone3four','4nineeightseven2','zoneight234','7pqrstsixteen']
+    # sample = ['eightwothree', 'two1nine','abcone2threexyz','xtwone3four','4nineeightseven2','zoneight234','7pqrstsixteen']
 
-    
     result = 0
-    for line in sample:
-        print(f'original line: {line}')
+    for line in lines:
         converted_line = is_num_str(line, nums_dict)
-        print(f'converted line: {converted_line}')
-        # result += solution(converted_line)
-        # print(f'num to be summed: {solution(converted_line)}')
-        print('')
+        result += solution(converted_line)
 
-    # print(result) 
+    print(result) 
 
 
 
