8c13c6a4c0fc37fcbee201e1a56ec831a03a306f
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2023-12-28 22:13:28 +0000

    rambling

--- solutions/two.py
+++ solutions/two.py
@@ -2,14 +2,24 @@ def part_one(input_game):
     
     allowed = {'red': 12, 'green': 13, 'blue': 14}
 
-    game_id = input_game[5]
-    games = []
+    # game_id = input_game[5]
+    valid_games = []
 
-    for game in input_game[8:].split('; ').strip():
+    for line in input_game:
+        for game in line[8:].split(' ;'):
+            print(game)
+
+        # for game in line: #input_game[8:].split('; '):
+        #     for s in game.split(', '):
+        #         check = s.split(' ')
+        #         if allowed[check[1]] >= int(check[0]):
+        #             valid_games.append(input_game[5])
+        #             print(check)
+
         #draws.append(draw)
-        print(game)
+        # print(game)
     
-    # return len(games)
+    return valid_games
 
 
 
@@ -26,8 +36,7 @@ def main():
 
     sample = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green','Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue','Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
 
-    for line in sample:
-        print(part_one(line))
+    print(part_one(sample))
 
 if __name__ == "__main__": 
     main()
