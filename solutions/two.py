5cbaeca194a7defc136841a45e3886794c63c9ce
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-11 22:18:17 +0000

    correct answer

--- solutions/two.py
+++ solutions/two.py
@@ -10,31 +10,28 @@ def part_one(input_game):
     valid_games = set()
 
     for line in input_game:
-        print(line)
+        # print(line)
         game_id = line.split(":")[0].replace("Game ", "")
-        print(game_id)
-        valid_games.add(int(game_id))
-        print(valid_games)
+        # print(game_id)
+        if game_id != "":
+            valid_games.add(int(game_id))
+        # print(valid_games)
 
         tosses = line.split(": ")[1]
         result = True
         for toss in tosses.split('; '):
-            colours = [(col.split(' ')) for col in toss.split(', ')]
-            # print('now in the for toss loop')
+            colours = [(col.split(' ')) for col in toss.split(', ') if len(col)]
             for colour in colours:
-                # print('now in the for colour loop')
                 check = {colour[1] : colour[0]}
-                print(check, int(colour[0]) <= allowed[colour[1]])
+                # print(check, int(colour[0]) <= allowed[colour[1]])
                 if int(colour[0]) > allowed[colour[1]]:
                     result = False 
-                    # print('move on to next game')
                     try:
                         valid_games.remove(int(game_id))
                     except:
                         pass
                     break
-                    # break
-        print('')
+        # print('')
 
     return sum(valid_games)
 
@@ -48,12 +45,13 @@ def main():
         lines = [] 
         for line in f: 
             lines.append(line.rstrip('\n')) 
-        
-        print(part_one(lines[:12]))
     
-    # print(lines[:])
+    # print(lines[98:103])
+    print(part_one(lines[:-1]))
+    
+    # print(lines[-4:-1])
 
-    # sample = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green','Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue','Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
+    # sample = ['Game 101: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green']#,'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue','Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
 
     # print(part_one(sample))
     # print(sample[0].split(":")[0].replace("Game ", ""))
