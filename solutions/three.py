527b54f8282b7d6ff340b72f1bbae8a2a4506ee5
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-11-14 22:07:47 +0000

    day 3 initial commit

---
+++ solutions/three.py
@@ -0,0 +1,23 @@
+def parse_input(input_data): 
+    arr = []
+    for row in input_data:
+        arr.append(row.rstrip('\n'))
+    return arr  
+
+def part_one(input_game):
+    pass 
+
+
+def part_two(input_game):
+    pass 
+
+
+def main():
+    
+    input_data = "..//inputs//three.txt"
+    input_array = parse_input(input_data)
+    return input_array
+
+
+if __name__ == "__main__":
+    print(main())
